16512ada9dc2eca211eb801eaad448d3c9846b75
yinchong <yin.chong@intellif.com>
2018-10-27 10:23:05 +0800

    Initial commit

---
+++ src/main/java/com/example/jdk9/Jdk9Application.java
@@ -0,0 +1,12 @@
+package com.example.jdk9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jdk9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jdk9Application.class, args);
+	}
+}
